677614ebc5e4c16945856c7e6b9b6bded813c5c0
monik-autofacets <monik.kacha@autofacets.com>
2026-03-30 16:40:09 +0530

    Test endpoints added

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const http = require("http");
+const mongoose = require("mongoose");
+
+const MONGO_URL = "mongodb://mongo:27017/testdb";
+
+async function connectWithRetry() {
+  while (true) {
+    try {
+      await mongoose.connect(MONGO_URL);
+      console.log("Connected to MongoDB");
+      break;
+    } catch (err) {
+      console.log("MongoDB not ready, retrying...");
+      await new Promise((res) => setTimeout(res, 2000));
+    }
+  }
+}
+
+connectWithRetry();
 
 const server = http.createServer((req, res) => {
-  res.end("CI/CD App Running");
+  res.end("App + MongoDB running");
 });
 
 server.listen(3000, () => {
